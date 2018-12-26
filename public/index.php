11c50dc0eef6525c4521b0c51b43318a6d31369a
Tinsh <kilofox2000@gmail.com>
2018-12-26 22:31:48 +0800

    refactor(public): refactor index.php

--- public/index.php
+++ public/index.php
@@ -15,44 +15,47 @@ unset($matches);
 
 /**
  * The directory in which your application specific resources are located.
- * The application directory must contain the config/kohana.php file.
+ * The application directory must contain the bootstrap.php file.
  *
- * @see  http://docs.kohanaphp.com/install#application
+ * @link https://kohana.top/guide/about.install#application
  */
-$application = '../application';
+$application = 'application';
 
 /**
  * The directory in which your modules are located.
  *
- * @see  http://docs.kohanaphp.com/install#modules
+ * @link https://kohana.top/guide/about.install#modules
  */
-$modules = '../modules';
+$modules = 'modules';
 
 /**
  * The directory in which the Kohana resources are located. The system
  * directory must contain the classes/kohana.php file.
  *
- * @see  http://docs.kohanaphp.com/install#system
+ * @link https://kohana.top/guide/about.install#system
  */
-$system = '../system';
+$system = 'system';
 
 /**
  * The default extension of resource files. If you change this, all resources
  * must be renamed to use the new extension.
  *
- * @see  http://docs.kohanaphp.com/install#ext
+ * @link https://kohana.top/guide/about.install#ext
  */
 define('EXT', '.php');
 
 /**
  * Set the PHP error reporting level. If you set this in php.ini, you remove this.
- * @see  http://php.net/error_reporting
+ * @link http://www.php.net/manual/errorfunc.configuration#ini.error-reporting
  *
  * When developing your application, it is highly recommended to enable notices
  * and strict warnings. Enable them by using: E_ALL | E_STRICT
  *
  * In a production environment, it is safe to ignore notices and strict warnings.
  * Disable them by using: E_ALL ^ E_NOTICE
+ *
+ * When using a legacy application with PHP >= 5.3, it is recommended to disable
+ * deprecated notices. Disable with: E_ALL & ~E_DEPRECATED
  */
 error_reporting(E_ALL | E_STRICT);
 
@@ -60,20 +63,20 @@ error_reporting(E_ALL | E_STRICT);
  * End of standard configuration! Changing any of the code below should only be
  * attempted by those with a working knowledge of Kohana internals.
  *
- * @see  http://docs.kohanaphp.com/bootstrap
+ * @link https://kohana.top/guide/using.configuration
  */
 // Set the full path to the docroot
-define('DOCROOT', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+define('DOCROOT', realpath(__DIR__ . '/..') . DIRECTORY_SEPARATOR);
 
-// Make the application relative to the docroot
+// Make the application relative to the docroot, for symlink'd index.php
 if (!is_dir($application) AND is_dir(DOCROOT . $application))
     $application = DOCROOT . $application;
 
-// Make the modules relative to the docroot
+// Make the modules relative to the docroot, for symlink'd index.php
 if (!is_dir($modules) AND is_dir(DOCROOT . $modules))
     $modules = DOCROOT . $modules;
 
-// Make the system relative to the docroot
+// Make the system relative to the docroot, for symlink'd index.php
 if (!is_dir($system) AND is_dir(DOCROOT . $system))
     $system = DOCROOT . $system;
 
@@ -85,21 +88,36 @@ define('SYSPATH', realpath($system) . DIRECTORY_SEPARATOR);
 // Clean up the configuration vars
 unset($application, $modules, $system);
 
-// Define the start time of the application
-define('KOHANA_START_TIME', microtime(TRUE));
+/**
+ * Define the start time of the application, used for profiling.
+ */
+if (!defined('KOHANA_START_TIME')) {
+    define('KOHANA_START_TIME', microtime(true));
+}
+
+/**
+ * Define the memory usage at the start of the application, used for profiling.
+ */
+if (!defined('KOHANA_START_MEMORY')) {
+    define('KOHANA_START_MEMORY', memory_get_usage());
+}
 
 // Bootstrap the application
 require APPPATH . 'bootstrap' . EXT;
 
+if (PHP_SAPI == 'cli') {
+    // Try and load minion
+    class_exists('Minion_Task') OR die('Please enable the Minion module for CLI support.');
+    set_exception_handler(['Minion_Exception', 'handler']);
 
-
-/**
- * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
- * If no source is specified, the URI will be automatically detected.
- */
-$cache = (extension_loaded('apc')) ? HTTP_Cache::factory('apc') : NULL;
-
-echo Request::factory(TRUE, $cache)
-    ->execute()
-    ->send_headers()
-    ->body();
+    Minion_Task::factory(Minion_CLI::options())->execute();
+} else {
+    /**
+     * Execute the main request. A source of the URI can be passed, eg: $_SERVER['PATH_INFO'].
+     * If no source is specified, the URI will be automatically detected.
+     */
+    echo Request::factory(true, [], false)
+        ->execute()
+        ->send_headers(true)
+        ->body();
+}
