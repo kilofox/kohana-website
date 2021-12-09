b2f68437c19306db17128a44ceb0d3d35e342ec4
Tinsh <kilofox2000@gmail.com>
2021-12-09 11:21:46 +0800

    fix(*): fix filename case insensitive

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -2,47 +2,62 @@
 
 // -- Environment setup --------------------------------------------------------
 // Load the core Kohana class
-require SYSPATH . 'classes/kohana/core' . EXT;
+require SYSPATH . 'classes/Kohana/Core' . EXT;
 
-if (is_file(APPPATH . 'classes/kohana' . EXT)) {
+if (is_file(APPPATH . 'classes/Kohana' . EXT)) {
     // Application extends the core
-    require APPPATH . 'classes/kohana' . EXT;
+    require APPPATH . 'classes/Kohana' . EXT;
 } else {
     // Load empty core extension
-    require SYSPATH . 'classes/kohana' . EXT;
+    require SYSPATH . 'classes/Kohana' . EXT;
 }
 
 /**
  * Set the default time zone.
  *
- * @see  http://kohana.top/guide/using.configuration
- * @see  http://php.net/timezones
+ * @link https://kohana.top/guide/using.configuration
+ * @link http://www.php.net/manual/timezones
  */
 date_default_timezone_set('America/Chicago');
 
 /**
  * Set the default locale.
  *
- * @see  http://kohana.top/guide/using.configuration
- * @see  http://php.net/setlocale
+ * @link https://kohana.top/guide/using.configuration
+ * @link http://www.php.net/manual/function.setlocale
  */
 setlocale(LC_ALL, 'en_US.utf-8');
 
 /**
  * Enable the Kohana auto-loader.
  *
- * @see  http://kohana.top/guide/using.autoloading
- * @see  http://php.net/spl_autoload_register
+ * @link https://kohana.top/guide/using.autoloading
+ * @link http://www.php.net/manual/function.spl-autoload-register
  */
 spl_autoload_register(['Kohana', 'auto_load']);
 
 /**
+ * Optionally, you can enable a compatibility auto-loader for use with
+ * older modules that have not been updated for PSR-0.
+ *
+ * It is recommended to not enable this unless absolutely necessary.
+ */
+//spl_autoload_register(['Kohana', 'auto_load_lowercase']);
+
+/**
  * Enable the Kohana auto-loader for unserialization.
  *
- * @see  http://php.net/spl_autoload_call
- * @see  http://php.net/manual/var.configuration.php#unserialize-callback-func
+ * @link http://www.php.net/manual/function.spl-autoload-call
+ * @link http://www.php.net/manual/var.configuration#unserialize-callback-func
  */
 ini_set('unserialize_callback_func', 'spl_autoload_call');
+
+/**
+ * Set the mb_substitute_character to "none"
+ *
+ * @link http://www.php.net/manual/function.mb-substitute-character.php
+ */
+mb_substitute_character('none');
 
 // -- Configuration and initialization -----------------------------------------
 
@@ -50,6 +65,11 @@ ini_set('unserialize_callback_func', 'spl_autoload_call');
  * Set the default language
  */
 I18n::lang('en-us');
+
+if (isset($_SERVER['SERVER_PROTOCOL'])) {
+    // Replace the default protocol.
+    HTTP::$protocol = $_SERVER['SERVER_PROTOCOL'];
+}
 
 /**
  * Set Kohana::$environment if a 'KOHANA_ENV' environment variable has been supplied.
@@ -70,9 +90,11 @@ if (isset($_SERVER['KOHANA_ENV'])) {
  * - string   index_file  name of your index file, usually "index.php"       index.php
  * - string   charset     internal character set used for input and output   utf-8
  * - string   cache_dir   set the internal cache directory                   APPPATH/cache
+ * - integer  cache_life  lifetime, in seconds, of items cached              60
  * - boolean  errors      enable or disable error handling                   true
  * - boolean  profile     enable or disable internal profiling               true
  * - boolean  caching     enable or disable internal caching                 false
+ * - boolean  expose      set the X-Powered-By header                        false
  */
 Kohana::init([
     'base_url' => '/',
