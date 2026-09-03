2d2d9d75bb6314b1b0f52067ff9fdda30775ec22
Loong <loong2460@gmail.com>
2026-09-04 00:54:23 +0800

    docs(application): add v3.5 docs and dynamic index

--- application/classes/View/Documentation/Index.php
+++ application/classes/View/Documentation/Index.php
@@ -30,4 +30,45 @@ class View_Documentation_Index extends Kostache_Layout
         return Route::url('home');
     }
 
+    /**
+     * Returns documentation versions data for the template loop.
+     *
+     * @return array
+     * @throws Kohana_Exception
+     */
+    public function docs(): array
+    {
+        return [
+            [
+                'version' => '3.5',
+                'url' => URL::base() . '3.5/guide/',
+                'label' => 'Kohana v3.5 Documentation',
+                'current' => true,
+            ],
+            [
+                'version' => '3.4',
+                'url' => URL::base() . '3.4/guide/',
+                'label' => 'Kohana v3.4 Documentation',
+                'current' => false,
+            ],
+            [
+                'version' => '3.3',
+                'url' => URL::base() . '3.3/guide/',
+                'label' => 'Kohana v3.3 Documentation',
+                'current' => false,
+            ],
+            [
+                'version' => '3.2',
+                'url' => URL::base() . '3.2/guide/',
+                'label' => 'Kohana v3.2 Documentation',
+                'current' => false,
+            ],
+            [
+                'version' => '3.1',
+                'url' => URL::base() . '3.1/guide/',
+                'label' => 'Kohana v3.1 Documentation',
+                'current' => false,
+            ],
+        ];
+    }
 }
